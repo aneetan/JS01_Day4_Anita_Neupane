b140a195f469f0ec6a0f986a2dad3b1901475a5e
aneetan <92221867+aneetan@users.noreply.github.com>
2021-11-18 11:17:29 +0545

    Add files via upload

---
+++ JS01_Day4_Anita_Neupane/Level1/task1.js
@@ -0,0 +1,26 @@
+// Declare variables, but assign values to those variables after declaration on the next line and 
+// show it on a popup and in a console. 
+
+var firstName;
+firstName = 'Anita';
+alert (firstName);
+console.log (firstName);
+
+var lastName;
+lastName = 'Neupane';
+alert (lastName);
+console.log (lastName);
+
+var age;
+age = '17';
+alert (age);
+console.log (age);
+
+var interest;
+interest = 'I am learning javascript.';
+alert (interest);
+console.log (interest);
+
+
+
+
